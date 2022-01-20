272b4cdbe400bc5a27c5183390508efc7570a633
siyan-david <siyanboladavid147@gmail.com>
2022-01-20 06:37:25 -0800

    Working Progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
-function App() {
+const App = () => {
   return (
     // fragment
     <>
-      <h1>Welcome to Shop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          {/* <h1>Welcome to Shop</h1> */}
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
